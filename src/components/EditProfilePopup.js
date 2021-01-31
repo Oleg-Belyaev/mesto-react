6d93866290eea3eb17a8d16c1de117542916e6f9
Oleg Belyaev <olegs.belyaev@yandex.ru>
2021-01-31 17:10:06 +0200

    Добавить валидацию форм и hook UseForm

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,46 +1,43 @@
 import React from 'react';
 import PopupWithForm from './PopupWithForm';
 import {CurrentUserContext} from '../contexts/CurrentUserContext';
+import { useForm } from '../hooks/useForm';
 
 function EditProfilePopup ({isOpen, onClose, onUpdateUser, onLoading}) {
   const currentUser = React.useContext(CurrentUserContext);
-  const [name, setName] = React.useState('');
-  const [description, setDescription] = React.useState('');
-
-  function handleNameChenge(e) {
-    setName(e.target.value);
-  }
-
-  function handleDescriptionChange(e) {
-    setDescription(e.target.value);
-  }
+  const { values, errors, isValid, handleChange, resetForm } = useForm();
+  
+  React.useEffect(() => {
+    if (currentUser) {
+      resetForm(currentUser, {}, true)
+    }
+  }, [currentUser, resetForm]);
 
   function handleSubmit (e) {
     e.preventDefault();
-
     onUpdateUser({
-      name,
-      about: description
+      name: values.name,
+      about: values.about
     })
   }
 
-  React.useEffect(() => {
-    setName(currentUser ? currentUser.name : '');
-    setDescription(currentUser ? currentUser.about : '');
-  },[currentUser]);
-
   return (
-    <PopupWithForm name="profile" title="Редактировать профиль" isOpen={isOpen} onClose={onClose} onSubmit={handleSubmit}>
+    <PopupWithForm name="profile" title="Редактировать профиль" 
+    isOpen={isOpen} onClose={onClose} onSubmit={handleSubmit}>
       <div className="popup__field">
-        <input type="text" className="popup__item" name="name" placeholder="Имя Фамилия" id="name-input" 
-        value={name} onChange={handleNameChenge}/>
+        <input type="text" className={`popup__item ${errors.name && "popup__item_type_error"}`} 
+        name="name" placeholder="Имя Фамилия" id="name-input" 
+        value={values.name || ''} onChange={handleChange} required minLength="2" maxLength="40"/>
+        <span className={`popup__item-error ${errors.name && "popup__item-error_active"}`}>{errors.name}</span>
       </div>
       <div className="popup__field">
-        <input type="text" className="popup__item" 
+        <input type="text" className={`popup__item ${errors.about && "popup__item_type_error"}`} 
         name="about" placeholder="Профессия" id="about-input" 
-        value={description} onChange={handleDescriptionChange}/>
+        value={values.about || ''} onChange={handleChange} required minLength="2" maxLength="200"/>
+        <span className={`popup__item-error ${errors.about && "popup__item-error_active"}`}>{errors.about}</span>
       </div>
-  <button type="submit" className="popup__submit" value="Сохранить">{onLoading ? 'Сохранить...' : 'Сохранить'}</button>        
+      <button type="submit" className={`popup__submit ${!isValid && "popup__submit_inactive"} `} 
+      value="Сохранить">{onLoading ? 'Сохранить...' : 'Сохранить'}</button>        
     </PopupWithForm>
   )
 }
